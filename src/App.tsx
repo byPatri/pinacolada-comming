ca32cec509234fe6236c4e82e7e60b57f15b9e71
byPatri <patrycja.krzeszowiec@gmail.com>
2022-06-16 22:45:25 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import img from './media/pinacolada.jpg'
 function App() {
   return (
     <div className='App'>
-      <span className='Text'>Comming soon ... at her own pace</span>
-      <img src={img} height={450} width={650} alt="pinacolada"/>
+      <span className='Text'>Comming soon ... at its own pace</span>
+      <img src={img} className='img' alt="pinacolada"/>
       <span className='Text'>You don't even know that you need it</span>
     </div>
   );
